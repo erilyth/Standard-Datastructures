86b529c1e73f3d5232e50a8632f587ef4b175749
Salman Shah <sbs.191197@gmail.com>
2016-10-11 19:49:19 +0530

    Inline Documentation added

--- Searching/LinearSeach.c
+++ Searching/LinearSeach.c
@@ -4,22 +4,31 @@
 
 //Linear Search function
 void linearSearch(int arr[], int n, int key) {
+  //Variable declaration
   int i;
+  
+  //Linear Search
   for(i = 0; i < n; i ++) {
     if(arr[i] == key) {
       printf("Key found at position %d",i);
       break;
     }
   }
+  
 }
 
 //Main Function
 int main() {
+  //Memory-efficient array
   int n;
   scanf("%d",&n);
   int arr[n],key,x;
+  
+  //Key for element to search
   printf("Enter the value of the key");
   scanf("%d",&key);
+  
+  //Function call
   linearSearch(arr,n,key);
   return 0;
 }
