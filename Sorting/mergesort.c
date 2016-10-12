485118ded302955b97923aff773f1c86fde5c985
Salman Shah <sbs.191197@gmail.com>
2016-10-12 16:45:25 +0530

    Sorting inline documentation added

--- Sorting/mergesort.c
+++ Sorting/mergesort.c
@@ -1,48 +1,68 @@
-#include<stdio.h>
-#include<stdlib.h>
+//Including header files
+#include <stdio.h>
+#include <stdlib.h>
 
-void merge(int A[], int start, int end)
-{
+//Merge sort function
+void merge(int A[], int start, int end) {
+	
+	//Variable declaration
 	int l,r,val,i,j,k;
 	int C[10000];
-	val=start; l=start; r=(start+end)/2+1;
-	while(l<=(start+end)/2 && r<=end)
-	{
+	
+	//Variable initialisation
+	val=start; 
+	l=start; 
+	r=(start+end)/2+1;
+
+	while(l<=(start+end)/2 && r<=end) {
 		if(A[l]<A[r])
 			C[val++]=A[l++];
 		else
 			C[val++]=A[r++];
 	}
+
 	while(l<=(start+end)/2)
 		C[val++]=A[l++];
 	while(r<=end)
 		C[val++]=A[r++];
+	
 	for(j=start;j<=end;j++)
 		A[j]=C[j];
+	
 	return;
 }
 
-void mergesort(int A[], int start, int end)
-{
-	if(end>start)
-	{
+void mergesort(int A[], int start, int end) {
+	
+	//Mergesort-Algorithm
+	if(end>start) {
 		mergesort(A,start,(start+end)/2);
 		mergesort(A,(start+end)/2+1,end);
 		merge(A,start,end);
 	}
+
 	return;
 }
 
-int main()
-{
-	int A[1000000];
+int main() {
+	
+	//Variable declaration
 	int i,j,a,n;
 	scanf("%d", &n);
-	for(i=0;i<n;i++)
+	//Memory efficient way to declare an array
+	int A[n];
+
+	//Inputting the elements of the array
+	for(i = 0; i < n; i ++)
 		scanf("%d", &A[i]);
+	
+	//Mergesort function call
 	mergesort(A,0,n-1);
-	for(i=0;i<n;i++)
+	
+	//SOrted array being displayed
+	for(i = 0; i < n; i ++)
 		printf("%d ", A[i]);
 	printf("\n");
+	
 	return 0;
 }
--- Sorting/randomizedquicksort.c
+++ Sorting/randomizedquicksort.c
@@ -1,54 +1,73 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+//Include header files
+#include <stdio.h>
+#include <stdlib.h>
+//Include time header file for random variable
+#include <time.h>
 
+//Global variable n declared
 int n;
 
-int partition(int A[], int start, int end, int pivot)
-{
+int partition(int A[], int start, int end, int pivot) {
+	
+	//Variable declaration
 	int i,j,a,b,c,piv,temp;
+	
+	//Variable initialisation
 	piv=A[pivot];
 	temp=A[pivot];
 	A[pivot]=A[end];
 	A[end]=temp;
 	int place=start;
-	for(i=start;i<=end;i++)
-	{	
-		if(A[i]<=piv)
-		{
+	
+	//Quick Sort implementation
+	for(i=start; i<=end; i++) {	
+		if(A[i]<=piv) {
 			temp=A[place];
 			A[place]=A[i];
 			A[i]=temp;
 			place++;
 		}
 	}
+
 	return place-1;
 }
 
-void quick(int A[], int start, int end)
-{
+void quick(int A[], int start, int end) {
+	
+
 	int newpiv,random;
-	if(start<end)
-	{
+	
+	if(start<end) {
+		//Random Variable initialisation
 		srand(time(NULL));
 		random=rand();
 		random=start+random%(end-start);
+		//Pivot declaration
 		newpiv=partition(A,start,end,random);
 		quick(A,start,newpiv-1);
 		quick(A,newpiv+1,end);
 	}
 }
 
-int main()
-{
-	int A[10000];
+//Main functionn declaration
+int main() {
+	//Variable initialisation
 	int i;
 	scanf("%d", &n);
-	for(i=0;i<n;i++)
+	//Memory efficient way to declare array
+	int A[n];
+	
+	//Inputting the values of the array
+	for(i=0; i<n; i++)
 		scanf("%d", &A[i]);
+	
+	//Quicksort function calling
 	quick(A,0,n-1);
+	
+	//Printing the Sorted values
 	for(i=0;i<n;i++)
 		printf("%d ", A[i]);
 	printf("\n");
+	
 	return 0;
 }
