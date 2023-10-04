6c315a1a9d0017a186dfb5ac547a7498947d1e19
Rishav Ranjan <rishavranjan943@gmail.com>
2023-10-05 00:56:22 +0530

    best case time O(n)

--- Sortings/bubblesort.c
+++ Sortings/bubblesort.c
@@ -14,14 +14,20 @@ int main()
         printf("%d. Enter element: ",i+1);
         scanf("%d",&data[i]);
     }
+    int flag;
     for(step=0;step<n-1;++step)
     {
+        flag=0;
     for(i=0;i<n-step-1;++i)
     {
         if(data[i]>data[i+1])   /* To sort in descending order, change > to < in this line. */
         {
             swap(&data[i],&data[i+1]);
+            flag=1;
         }
+    }
+    if(flag==0){
+        break;
     }
     }
     printf("In ascending order: ");
